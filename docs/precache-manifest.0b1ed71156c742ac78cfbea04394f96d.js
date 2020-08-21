4b6fb894e4c4ddb87bd30a2d0f611ebe73e103c6
Manuel Marquez <mmarquez@rootech.services>
2020-08-21 02:29:37 -0400

    Fist Commit

---
+++ docs/precache-manifest.0b1ed71156c742ac78cfbea04394f96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84aca48e50e1930109d46a1f58456ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2a70bfb7e64fb3778fb",
+    "url": "/static/css/main.71adc134.chunk.css"
+  },
+  {
+    "revision": "1033910752db7724b58e",
+    "url": "/static/js/2.58cc56e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58cc56e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a70bfb7e64fb3778fb",
+    "url": "/static/js/main.2ffd2474.chunk.js"
+  },
+  {
+    "revision": "a62a70cb59047f164f84",
+    "url": "/static/js/runtime-main.1fdb1c7d.js"
+  }
+]);
